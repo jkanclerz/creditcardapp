f7049e3f17e62bb11e9ea0f4eb1b4ca072b0038f
Jakub Erazmus <jerazmus@o2.pl>
2019-10-24 10:45:45 +0200

    Test Class added

--- src/main/java/pl/krakow/uek/pp5/jkan/creditcardapp/model/App.java
+++ src/main/java/pl/krakow/uek/pp5/jkan/creditcardapp/model/App.java
@@ -1,3 +1,5 @@
+package pl.krakow.uek.pp5.jkan.creditcardapp.model;
+
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello world");
--- src/test/java/pl/krakow/uek/pp5/jkan/creditcardapp/model/CreditCardTest.java
+++ src/test/java/pl/krakow/uek/pp5/jkan/creditcardapp/model/CreditCardTest.java
@@ -1,5 +1,20 @@
 package pl.krakow.uek.pp5.jkan.creditcardapp.model;
 
-public class CreditCardTest {
+import java.math.BigDecimal;
+import org.junit.Assert;
+import org.junit.Test;
 
+public class CreditCardTest {
+    public static final int NEW_CREDIT_LIMIT = 2000;
+
+    @Test
+    public void itAllowAssignLimitToCreditCard() {
+        //Arrange // Given
+        CreditCard card = new CreditCard("1234-5678");
+        //Act // When
+        card.assignLimit(BigDecimal.valueOf(NEW_CREDIT_LIMIT));
+        //Assert // Them // Expect
+        Assert.assertTrue(card.getLimit().equals(BigDecimal.valueOf(NEW_CREDIT_LIMIT)));
+
+    }
 }
